7bfe2134688b8760065c8d430aa413c367fe759a
Andrew <andrewcarlospotts@gmail.com>
2025-05-13 08:39:18 -0600

    initial commit

---
+++ Module1/Module1.java
@@ -0,0 +1,9 @@
+class Module1 {
+    public static void main(String[] args) {
+        System.out.println("First name: John");
+        System.out.println("Last name: Smith");
+        System.out.println("Street address: 123 Main Street");
+        System.out.println("City: Salt Lake City");
+        System.out.println("Zip code: 12345");
+    }
+}
