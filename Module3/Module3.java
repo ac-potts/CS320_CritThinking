5b801ecf803385ecbcb830e0413f2857b4b74510
Andrew <andrewcarlospotts@gmail.com>
2025-05-15 09:50:28 -0600

    Module 3

---
+++ Module3/Module3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Module3 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double weeklyIncome;
+        double taxRate;
+        double taxWithholdings;
+
+        System.out.println("Enter your weekly income:");
+        weeklyIncome = scnr.nextDouble();
+
+        if (weeklyIncome < 500) {
+            taxRate = 0.10;
+        } else if (weeklyIncome < 1500) {
+            taxRate = 0.15;
+        } else if (weeklyIncome < 2500) {
+            taxRate = 0.20;
+        } else {
+            taxRate = 0.30;
+        }
+
+        taxWithholdings = weeklyIncome * taxRate;
+        System.out.println("Weekly income: " + weeklyIncome);
+        System.out.println("Tax rate: " + taxRate);
+        System.out.println("Tax withholdings: " + taxWithholdings);
+    }
+
+}
